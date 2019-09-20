927a386e3928d313b76353f85a1bf89b78a029f0
Evan Pratten <ewpratten@gmail.com>
2019-09-20 11:45:11 -0400

    created arduino project

---
+++ spacecraft/obc_source/sketch_sep20a/sketch_sep20a.ino
@@ -0,0 +1,23 @@
+/**
+ * Spike Source code
+ * By: Evan Pratten
+ * 
+ * Sensors:
+ *  - Photocell
+ *  - Temprature
+ *  
+ * Output:
+ *  - Radio tx
+ * 
+ */
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
